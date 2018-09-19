d9a1a0fd4fceb9d2eaf012e9f237b7cef3e82371
Laurel Wright <lwrocks@Laurels-MBP.fritz.box>
2018-09-19 18:51:12 +0200

    Added basic form to index.html

--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -5,13 +5,13 @@ fixture('Index page')
 
 test('Should contain a form field', async (t) => {
     const field = Selector('.hash');
-  
+
     await t.expect(field.exists).ok();
   });
   test('Should contain a submit button', async (t) => {
-    const field = Selector('.submit');
-  
-    await t.expect(field.exists).ok();
+    const field2 = Selector('.submit');
+
+    await t.expect(field2.exists).ok();
   });
   // test('Should send a GET request when submit button is clicked', async (t) => {
     // write code
